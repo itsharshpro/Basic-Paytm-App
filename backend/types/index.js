52a32c1d0bebb461b1d2f232b3a7529457eae422
itsharshpro <harshpro2004@gmail.com>
2024-09-29 06:07:02 +0530

    initial commit

---
+++ backend/types/index.js
@@ -0,0 +1,25 @@
+const zod = require('zod')
+
+const signupSchema = zod.object({
+    firstName: zod.string(),
+    lastName: zod.string(),
+    username: zod.string().email(),
+    password: zod.string()
+})
+
+const signinSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+});
+
+const updateSchema = zod.object({
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+    password: zod.string().optional()
+})
+
+module.exports = {
+    signupSchema,
+    signinSchema,
+    updateSchema
+}
